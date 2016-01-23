3391f4cff73ae1561f0058be7d18b981ff9877d9
kardagan <geoffrey.laurent@gmail.com>
2016-01-23 22:15:54 +0100

    génération de l'id et envoie dans le header

--- PhpDevTools.class.php
+++ PhpDevTools.class.php
@@ -2,8 +2,21 @@
 
 class PhpDevTools {
 
+    /* id de la page qui est envoyé dans le header*/
+    private static $id = null;
+
     public function __callStatic($name, $arguments) {
 
     }
 
+    public static function init() {
+        self::getId();
+    }
+
+    private static function getId() {
+        if ( self::$id === null ) {
+            self::$id = uniqid();
+            header("phpdevtools:".self::$id);
+        }
+    }
 }
