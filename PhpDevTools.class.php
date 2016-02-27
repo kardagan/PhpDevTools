b1c3947807ab0f3318764270ff19ee386f57b535
kardagan <geoffrey.laurent@gmail.com>
2016-02-28 00:19:31 +0100

    mise en place de la sortie json

--- PhpDevTools.class.php
+++ PhpDevTools.class.php
@@ -9,7 +9,7 @@ class PhpDevTools {
     private static $redis_connection = null;
     private static $redis_host = '127.0.0.1';
     private static $redis_port = 6379;
-    private static $redis_database = 1;
+    private static $redis_database = 0;
     private static $redis_ttl = 60;
 
     public static function __callStatic($name, $arguments) {
@@ -35,6 +35,17 @@ class PhpDevTools {
         self::getId();
     }
 
+    public static function getJson( $psId ) {
+        header('Access-Control-Allow-Origin:*');
+        header('Content-Type:application/json');
+        $aDebug = self::getRedisConnection()->lrange(self::getKey($psId),0,-1);
+        foreach ( $aDebug as &$sDebug ) {
+            $sDebug = json_decode($sDebug);
+        }
+        echo json_encode($aDebug);
+        die();
+    }
+
     private static function getId() {
         if ( self::$id === null ) {
             self::$id = uniqid();
@@ -43,8 +54,17 @@ class PhpDevTools {
         return self::$id;
     }
 
+    private static function getKey ( $psId ) {
+        return 'phpdevtools:' . $psId;
+    }
+
     private static function record( $data ) {
-        $key = 'phpdevtools:'.self::getId();
+        $key = self::getKey(self::getId());
+        self::getRedisConnection()->lpush($key,json_encode($data));
+        self::getRedisConnection()->expire($key,self::$redis_ttl);
+    }
+
+    private static function getRedisConnection () {
         if ( self::$redis_connection === null ) {
             self::$redis_connection = new \Predis\Client([
                 'host'=>self::$redis_host,
@@ -52,8 +72,7 @@ class PhpDevTools {
             ]);
             self::$redis_connection->select(self::$redis_database);
         }
-        self::$redis_connection->lpush($key,json_encode($data));
-        self::$redis_connection->expire($key,self::$redis_ttl);
+        return self::$redis_connection;
     }
 
 }
